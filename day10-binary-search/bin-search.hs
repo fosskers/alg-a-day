9590c9ce2fe1870e4774a82b939b49b7b141a0f4
Colin Woodbury <colingw@gmail.com>
2011-08-10 22:31:50 +0900

    Algorith-a-Day : Day 10 : Binary Search : edit2
    - Haskell version was returning a Bool... don't ask why.

--- day10-binary-search/bin-search.hs
+++ day10-binary-search/bin-search.hs
@@ -1,10 +1,10 @@
 -- Binary Search in Haskell.
 
-binSearch :: Integral a => a -> [a] -> Bool
-binSearch n [] = False
+binSearch :: Integral a => a -> [a] -> Int
+binSearch n [] = (-1)
 binSearch n ns | n < midval = binSearch n $ take midpos ns
                | n > midval = binSearch n $ drop (midpos + 1) ns       
-               | otherwise  = True
+               | otherwise  = midpos
                where
                  midval = ns !! midpos
                  midpos = length ns `div` 2
